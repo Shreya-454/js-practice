c16ffd343c0baea50a019bd12383b2dd1c0dcf5f
Shreya-454 <bshreya932@GMAIL.COM>
2024-10-01 15:32:18 +0530

    for loop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Forloop from './components/Forloop';
 import RangeForm from './components/RangeForm';
+import RangeLoop from './components/RangeLoop';
 
 function App() {
   return (
     <div>
       <RangeForm/>
       <Forloop/>
+      <RangeLoop/>
     </div>
   );
 }
--- src/components/RangeLoop.jsx
+++ src/components/RangeLoop.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 const RangeLoop = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    function Table() {
+        let results = [];
+        for (let i = 2; i <= 10; i++) {
+            let table = [];
+            for (let a = 1; a <= 10; a++) {
+                table.push(a * i);
+            }
+            results.push(table);
+        }
+        return results;
+    }
 
-export default RangeLoop
+    return (
+        <div className='container max-w-[1140px] mx-auto mt-10'>
+            <div className='flex gap-4 justify-center'>
+                {Table().map((table, index) => (
+                    <div key={index} className='flex flex-col items-center'>
+                        <h3 className='text-2xl text-blue-600'>Table of {index + 2}</h3>
+                        {table.map((result, i) => (
+                            <p key={i} className='text-2xl text-red-600'>{result}</p>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default RangeLoop;
