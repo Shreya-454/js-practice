fb84ce49e8c7c139e4ad669ea949b9c3273aa2ac
Shreya-454 <bshreya932@GMAIL.COM>
2024-10-01 12:38:10 +0530

    first

--- src/components/RangeForm.jsx
+++ src/components/RangeForm.jsx
@@ -90,7 +90,7 @@ function RangeForm() {
           <div>
             <button
               type="submit"
-              className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              className="w-full bg-green-500 text-white py-2 px-4 rounded-md hover:bg-black"
             >
               Calculate
             </button>
